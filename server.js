3026ca7f5b120474ce82c2010c7d91a4309354a2
Diego Chavez <diegchav.me@gmail.com>
2020-04-19 01:29:36 -0500

    Add error handler for custom 404 messages and move error handlers to middleware module

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-const { STATUS_CODES } = require('http')
 
 const baseRouter = require('./routes')
+const middleware = require('./middleware')
 
 const app = express()
 
@@ -20,29 +20,8 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api', baseRouter)
 
 /* Error handlers */
-app.use((err, req, res, next) => {
-    console.error(err)
-
-    if (err.name !== 'ValidationError') return next(err)
-
-    const errors = Object.values(err.errors).map(e => {
-        const { path, message } = e
-        return {
-            [path]: message
-        }
-    })
-
-    res.status(400).json(errors)
-})
-
-app.use((err, req, res, next) => {
-    console.error(err)
-    
-    if (res.headersSent) return next(err)
-
-    const statusCode = err.statusCode || 500
-    const errorMessage = STATUS_CODES[statusCode] || 'Internal Error'
-    res.status(statusCode).json({ error: errorMessage })
-})
+app.use(middleware.handleValidationError)
+app.use(middleware.handleError)
+app.use(middleware.handleNotFound)
 
 module.exports = app
