963b54970483dcd89b9ee2c5878569335600249c
Diego Chavez <diegchav.me@gmail.com>
2020-05-07 18:36:30 -0500

    Add avatar field to user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,6 @@
 import { modelOptions, pre, prop, arrayProp, Ref, getModelForClass } from '@typegoose/typegoose'
 import bcrypt from 'bcrypt'
+import crypto from 'crypto'
 
 const saltRounds = bcrypt.genSaltSync(Number(process.env.SALT_ROUNDS) || 10)
 
@@ -26,6 +27,14 @@ const saltRounds = bcrypt.genSaltSync(Number(process.env.SALT_ROUNDS) || 10)
 
 @pre<User>('save', async function() {
     this.password = await bcrypt.hash(this.password, saltRounds)
+
+    // Generate avatar from email
+    const gravatarUrl = process.env.GRAVATAR_URL
+    const gravatarHexPattern = process.env.GRAVATAR_HEX_PATTERN
+    const gravatarDefault = process.env.GRAVATAR_DEFAULT
+    const regexPattern = new RegExp(gravatarHexPattern!)
+    const emailHashed = crypto.createHash('md5').update(this.email).digest('hex')
+    this.avatar = gravatarUrl?.replace(regexPattern, emailHashed) || gravatarDefault!
 })
 
 export class User {
@@ -48,6 +57,9 @@ export class User {
 
     @prop({ default: '' })
     public bio?: string
+
+    @prop({ default: '' })
+    public avatar!: string
 
     @arrayProp({ itemsRef: User })
     public following!: Ref<User>[]
