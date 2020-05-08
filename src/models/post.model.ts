19a6b8038659e01d86cca5299a5412b491b0aed4
Diego Chavez <diegchav.me@gmail.com>
2020-05-07 23:54:59 -0500

    Create a fromNow virtual field on post model for better formatting the time when the post was created

--- src/models/post.model.ts
+++ src/models/post.model.ts
@@ -1,15 +1,18 @@
 import { modelOptions, prop, getModelForClass, Ref } from '@typegoose/typegoose'
+import moment from 'moment'
 
 import { User } from './user.model'
 
 @modelOptions({
     schemaOptions: {
         toObject: {
+            virtuals: true,
             transform: (doc, ret, options) => {
                 delete ret.__v
             }
         },
         toJSON: {
+            virtuals: true,
             transform: (doc, ret, options) => {
                 delete ret.__v
             }
@@ -29,6 +32,10 @@ export class Post {
 
     @prop({ default: Date.now })
     public createdAt?: Date
+
+    public get fromNow() {
+        return moment(this.createdAt).fromNow()
+    }
 }
 
 export const PostModel = getModelForClass(Post)
